975e51b9e9d28854bfca90e39c4fc510b487c812
HaldeFua <haroldareiza89@gmail.com>
2025-03-17 16:14:37 -0500

    Contents of the abstract class Products

--- Products.cs
+++ Products.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Retail_Sales
 {
-    internal class Products
+    public abstract class Products
     {
+
+        public int ID { get; protected set; }
+        public string Name { get; protected set; }
+        public decimal Price { get; protected set; }
+        public decimal Tax { get; protected set; } = 0.19m;
+
+        private decimal _discount;
+
+        public Products()
+        {
+        }
+
+        public Products(int iD, string name, decimal price)
+        {
+            ID = iD;
+            Name = name;
+            Price = price;
+        }
+
+
     }
 }
